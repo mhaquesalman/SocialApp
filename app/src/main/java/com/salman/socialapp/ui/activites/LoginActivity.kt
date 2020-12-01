e298cdf4404133fd030728b25e0ec288c14a7b72
mhaquesalman <mhaquesalman@gmail.com>
2020-12-01 18:22:46 +0600

    Initial commit

---
+++ app/src/main/java/com/salman/socialapp/ui/activites/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.salman.socialapp.ui.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.salman.socialapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
