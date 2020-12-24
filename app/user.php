6fe79150a782a5bc606d194e9ca2f2e918b3f35d
mhaquesalman <mhaquesalman@gmail.com>
2020-12-24 21:30:44 +0600

    newsfeed feature added

--- app/user.php
+++ app/user.php
@@ -202,7 +202,8 @@ $app->post('/uploadImage',function($request,  $response,  $args) {
 
 // Api for search
 $app->get('/search',function($request,  $response,  $args){
-	include __DIR__ .'/../bootstrap/dbconnection.php';
+    include __DIR__ .'/../bootstrap/dbconnection.php';
+    
 	$keyword = $request->getQueryParams()['keyword'];
 
 	$query = $pdo->prepare("SELECT * from users where name like '%$keyword%' limit 10");
@@ -224,6 +225,45 @@ $app->get('/search',function($request,  $response,  $args){
 
 });
 
+//Api for personalized timeline
+$app->get('/getnewsfeed',function($request,  $response,  $args){
+	include __DIR__ . '/../bootstrap/dbconnection.php';
+  
+   $uid = $request->getQueryParams()['uid'];
+   $limit = $request->getQueryParams()['limit'];
+   $offset = $request->getQueryParams()['offset'];
+  
+   $query = $pdo->prepare("
+						   SELECT posts.*, users.* from `timeline`
+						   INNER JOIN `posts` on timeline.postId = posts.postId
+						   INNER JOIN `users` on posts.postUserId = users.uid
+						   WHERE timeline.whoseTimeLine = :uid
+						   ORDER By timeline.statusTime DESC
+						   LIMIT $limit OFFSET $offset
+						   "
+					   );
+
+   $query->bindParam(':uid', $uid, PDO::PARAM_STR);		 
+   $query->execute();
+
+   $errorData = $query->errorInfo();
+   if($errorData[1]){
+	   return checkError($response, $errorData);
+   }
+  
+    $posts= $query->fetchAll(PDO::FETCH_OBJ);
+    
+	$output['status']  = 200;
+	$output['message'] = "Newsfeed Loaded Successfully";
+	$output['posts'] = $posts;
+
+
+	$payload = json_encode($output);
+	$response->getBody()->write($payload);
+	return $response->withHeader('Content-Type', 'application/json')->withStatus(200);	
+	  
+		  
+});
 
 function checkRequest($userId,$profileId){
 	include __DIR__ . '/../bootstrap/dbconnection.php';
